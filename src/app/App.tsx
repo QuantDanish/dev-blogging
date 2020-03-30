b0a4cc6896317a58aae05453e047172cf9b5fc81
QuantDanish <mohd.danish@quovantis.com>
2020-03-30 11:15:59 +0530

    added footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-
 import Header from './component/header/header';
 import AppRoute from './component/appRoute/appRoute';
 import './app.scss';
+import Footer from './component/footer/FooterComponent';
 
 const App: React.FC = () => {
   return (
     <div className="app-component">
       <BrowserRouter>
         <Header />
-          <AppRoute />
+        <AppRoute />
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
-// import {Provider} from 'react-redux';
 
 import App from "./app/App";
 
@@ -8,8 +7,5 @@ import App from "./app/App";
 import './index.scss';
 
 const  wrapper  = <App />
-{/* <Provider store={store}>
-                    <App />
-                  </Provider> */}
 
 ReactDOM.render( wrapper, document.getElementById('root'));
